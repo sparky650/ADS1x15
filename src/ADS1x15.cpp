9301bd41c6b50c81e19e1e02d128fb8da8e949f7
Keegan Morrow <meserv@gmail.com>
2017-04-17 18:41:55 -0400

    Bump to 0.0.2
    Most functions implimented, tested on real hardware

--- src/ADS1x15.cpp
+++ src/ADS1x15.cpp
@@ -1,27 +1,195 @@
 #include "ADS1x15.h"
 
+/**
+ * @brief Set the calibration factor for calculating the voltage or current input
+ *
+ * @param calibration Correction factor
+ */
+void ADS1x15::setCalibration(float calibration)
+{
+	this->calibration = calibration;
+}
+
+/**
+ * @brief Calculate the calibration factor for calculating the voltage or current input
+ *
+ * @param r1 First resistor in the resistor divider
+ * @param r2 Second resistor in the resistor divider
+ */
+void ADS1x15::setCalibration(float r1, float r2)
+{
+	if (r2 > 0.0) { calibration = (r1 + r2) / r2; }
+}
+
+/**
+ * @brief Set the gain value for the programmable gain amplifier
+ *
+ * @param currentGain Gain value from ADS1x15_GAIN_t
+ */
+void ADS1x15::setGain(ADS1x15_GAIN_t currentGain)
+{
+	this->currentGain = currentGain;
+	configRegister &= ~(uint16_t)ADS1x15_GAIN_MASK;
+	configRegister |= (uint16_t)currentGain;
+}
+
+/**
+ * @brief Get the current full scale value in V
+ *
+ * @return Voltage based on the current gain and calibration factor
+ */
+float ADS1x15::getFullScaleV()
+{
+	float value = 0.0;
+	if (currentGain == GAIN_23) {value = 6.144;}
+	else if (currentGain == GAIN_1) {value = 4.096;}
+	else if (currentGain == GAIN_2) {value = 2.048;}
+	else if (currentGain == GAIN_4) {value = 1.024;}
+	else if (currentGain == GAIN_8) {value = 0.512;}
+	else if (currentGain == GAIN_16) {value = 0.256;}
+	return value * calibration;
+}
+
+/**
+ * @brief Set the mode of the comparator.
+ *
+ * @param compCfg Configuration to set
+ */
+void ADS1x15::setComparatorMode(ADS1x15_COMP_MODE_t compCfg)
+{
+	configRegister &= ~(uint16_t)ADS1x15_COMP_MODE_MASK;
+	configRegister |= (uint16_t)compCfg;
+}
+
+/**
+ * @brief Set the polarity of the comparator.
+ *
+ * @param compCfg Configuration to set
+ */
+void ADS1x15::setComparatorPolarity(ADS1x15_COMP_POL_t compCfg)
+{
+	configRegister &= ~(uint16_t)ADS1x15_COMP_POL_MASK;
+	configRegister |= (uint16_t)compCfg;
+}
+
+/**
+ * @brief Set the latching mode of the comparator.
+ *
+ * @param compCfg Configuration to set
+ */
+void ADS1x15::setComparatorLatch(ADS1x15_COMP_LAT_t compCfg)
+{
+	configRegister &= ~(uint16_t)ADS1x15_COMP_LAT_MASK;
+	configRegister |= (uint16_t)compCfg;
+}
+
+/**
+ * @brief Read an analog value
+ *
+ * @param m The configuration of the MUX
+ * @return The converted value
+ */
+int16_t ADS1x15::analogRead(ADS1x15_MUX_t mux)
+{
+	uint16_t result;
+	configRegister &= ~(uint16_t)ADS1x15_MUX_MASK;
+	configRegister |= (uint16_t)mux;
+	configRegister |= ADS1x15_OS;
+	writeRegister(CONFIG_REG, configRegister);
+	delayMicroseconds(conversionDelay);
+	result = shiftConversion(readRegister(CONVERSION_REG));
+	if (result > getFullScaleBits()) { return (int16_t)(result | 0x8000); }
+	else { return (int16_t)result; }
+}
+
+/**
+ * @brief Read an analog value
+ *
+ * @param ch The input channel to read
+ * @return The converted value
+ */
+uint16_t ADS1x15::analogRead(uint8_t ch)
+{
+	if (ch == 0) { return analogRead(SE0); }
+	else if (ch == 1) { return analogRead(SE1); }
+	else if (ch == 2) { return analogRead(SE2); }
+	else if (ch == 3) { return analogRead(SE3); }
+	return 0;
+}
+
+/**
+ * @brief Read an input and calculate the voltage based on the current gain settings
+ *
+ * @param ch The input channel to read
+ * @return The converted value in V
+ */
+float ADS1x15::analogReadVoltage(uint8_t ch)
+{
+	return getFullScaleV() * ((float)analogRead(ch) / (float)getFullScaleBits());
+	// return ((float)analogRead(ch) / (float)getFullScaleBits());
+}
+
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param ch The input channel to read
+ * @param r Burden resistor value in ohms
+ *
+ * @return The converted value in mA
+ */
+float ADS1x15::analogReadCurrent(uint8_t ch, float r)
+{
+	if (r > 0.0) { return analogReadVoltage(ch) / r; }
+	else { return 0.0; }
+}
+
+/**
+ * @brief Read the output from a 4-20mA device in %
+ *
+ * @param ch The input channel to read
+ * @param r Burden resistor value in ohms
+ *
+ * @return The converted value in %
+ */
+float ADS1x15::analogRead420(uint8_t ch, float r)
+{
+	return (analogReadCurrent(ch, r) - 4.0) / 16.0;
+}
+
+/**
+ * @brief Set the conversion rate in samples per second
+ *
+ * @param dataRate One of the rate settings from ADS1115_DR_t
+ */
 void ADS1115::setDataRate(ADS1115_DR_t dataRate)
 {
-
+	configRegister &= ~(uint16_t)ADS1x15_DR_MASK;
+	configRegister |= (uint16_t)dataRate;
+	if (dataRate == ADS1115_DR_8) { conversionDelay = 125400; }
+	if (dataRate == ADS1115_DR_16) { conversionDelay = 62900; }
+	if (dataRate == ADS1115_DR_32) { conversionDelay = 31650; }
+	if (dataRate == ADS1115_DR_64) { conversionDelay = 16025; }
+	if (dataRate == ADS1115_DR_128) { conversionDelay = 8213; }
+	if (dataRate == ADS1115_DR_250) { conversionDelay = 4400; }
+	if (dataRate == ADS1115_DR_475) { conversionDelay = 2505; }
+	if (dataRate == ADS1115_DR_860) { conversionDelay = 1563; }
 }
 
+/**
+ * @brief Set the conversion rate in samples per second
+ *
+ * @param dataRate One of the rate settings from ADS1015_DR_t
+ */
 void ADS1015::setDataRate(ADS1015_DR_t dataRate)
 {
-
-}
-
-
-uint16_t ADS1x15::analogRead(uint8_t)
-{
-	return 0;
-}
-
-uint16_t ADS1x15::analogReadVoltage(uint8_t)
-{
-	return 0;
-}
-
-uint16_t ADS1x15::analogReadCurrent(uint8_t)
-{
-	return 0;
+	configRegister &= ~ADS1x15_DR_MASK;
+	configRegister |= (uint16_t)dataRate;
+	if (dataRate == ADS1015_DR_128) { conversionDelay = 8213; }
+	if (dataRate == ADS1015_DR_250) { conversionDelay = 4400; }
+	if (dataRate == ADS1015_DR_490) { conversionDelay = 2441; }
+	if (dataRate == ADS1015_DR_920) { conversionDelay = 1487; }
+	if (dataRate == ADS1015_DR_1600) { conversionDelay = 1025; }
+	if (dataRate == ADS1015_DR_2400) { conversionDelay = 817; }
+	if (dataRate == ADS1015_DR_3300) { conversionDelay = 703; }
 }
--- src/ADS1x15.h
+++ src/ADS1x15.h
@@ -1,7 +1,7 @@
 /**
  * @file ADS1115.h
  * @author Keegan Morrow
- * @version 0.0.1
+ * @version 0.0.2
  *
  */
 
@@ -14,12 +14,44 @@
 
 enum ADS1x15_Register_t
 {
-	CONVERSION = 0x00,
-	CONFIG = 0x01,
-	LOW_THRESH = 0x02,
-	HI_THRESH = 0x03
+	CONVERSION_REG = 0x00,
+	CONFIG_REG = 0x01,
+	LOW_THRESH_REG = 0x02,
+	HI_THRESH_REG = 0x03
 };
 
+static const uint16_t ADS1x15_OS = 1 << 15;
+
+static const uint16_t ADS1x15_MUX_MASK = (0x07) << 12;
+enum ADS1x15_MUX_t
+{
+	DIF01 = (0x0 << 12),
+	DIF03 = (0x1 << 12),
+	DIF13 = (0x2 << 12),
+	DIF23 = (0x3 << 12),
+	SE0 = (0x4 << 12),
+	SE1 = (0x5 << 12),
+	SE2 = (0x6 << 12),
+	SE3 = (0x7 << 12)
+};
+
+static const uint16_t ADS1x15_GAIN_MASK = (0x7) << 9;
+enum ADS1x15_GAIN_t
+{
+	GAIN_23 = (0x0 << 9),
+	GAIN_1 = (0x1 << 9),
+	GAIN_2 = (0x2 << 9),
+	GAIN_4 = (0x3 << 9),
+	GAIN_8 = (0x4 << 9),
+	GAIN_16 = (0x5 << 9)
+};
+
+static const uint16_t ADS1x15_MODE_MASK = 1 << 8;
+enum ADS1x15_MODE_t
+{
+	CONTINUOUS_CONV = 0x0 << 8,
+	SINGLE_SHOT = 0x1 << 8
+};
 
 static const uint16_t ADS1x15_DR_MASK = (0x7) << 5;
 enum ADS1115_DR_t
@@ -33,7 +65,6 @@ enum ADS1115_DR_t
 	ADS1115_DR_475 = (0x6 << 5),
 	ADS1115_DR_860 = (0x7 << 5)
 };
-
 enum ADS1015_DR_t
 {
 	ADS1015_DR_128 = (0x0 << 5),
@@ -45,28 +76,46 @@ enum ADS1015_DR_t
 	ADS1015_DR_3300 = (0x6 << 5)
 };
 
-
-static const uint16_t ADS1x15_GAIN_MASK = (0x7) << 9;
-enum ADS1x15_GAIN_t
+static const uint16_t ADS1x15_COMP_MODE_MASK = 1 << 4;
+enum ADS1x15_COMP_MODE_t
 {
-	GAIN_23 = (0x0 << 9),
-	GAIN_1 = (0x1 << 9),
-	GAIN_2 = (0x2 << 9),
-	GAIN_4 = (0x3 << 9),
-	GAIN_8 = (0x4 << 9),
-	GAIN_16 = (0x5 << 9)
+	STANDARD_COMP = 0x0 << 4,
+	WINDOW_COMP = 0x1 << 4
+};
+
+static const uint16_t ADS1x15_COMP_POL_MASK = 1 << 3;
+enum ADS1x15_COMP_POL_t
+{
+	ACTIVE_LOW = 0x0 << 3,
+	ACTIVE_HIGH = 0x1 << 3
+};
+
+static const uint16_t ADS1x15_COMP_LAT_MASK = 1 << 2;
+enum ADS1x15_COMP_LAT_t
+{
+	NONLATCHING_COMP = 0x0 << 2,
+	LATCHING_COMP = 0x1 << 2
+};
+
+static const uint16_t ADS1x15_QUE_MASK = 0x3;
+enum ADS1x15_QUE_t
+{
+	QUE_ONE = 0x0,
+	QUE_TWO = 0x1,
+	QUE_FOUR = 0x2,
+	QUE_DISABLE = 0x3
 };
 
 typedef ADS1x15_GAIN_t ADS1015_GAIN_t;
 typedef ADS1x15_GAIN_t ADS1115_GAIN_t;
 
+static const uint16_t defaultConfig = 0x8583; // default startup state (from the datasheet)
+
 static const uint8_t defaultAddress = 0x48;
 
-static const float fullScaleV[] =
-{ 6.144, 4.096, 2.048, 1.024, 0.512, 0.256 };
-
-
-
+/**
+ * @brief Foundation class for the ADS1015 and ADS1115 ADCs
+ */
 class ADS1x15: public wireUtil<ADS1x15_Register_t, uint16_t>
 {
 public:
@@ -75,35 +124,88 @@ public:
 	{
 		timeoutTime = 1000UL;
 		timeoutFlag = false;
+		calibration = 1.0;
+		configRegister = defaultConfig;
+		setGain(GAIN_2);
 	}
 	void begin() {begin(defaultAddress);}
-
-	uint8_t addressIndex(uint8_t a) {return a + defaultAddress;}
-
+	inline uint8_t addressIndex(uint8_t a) {return a + defaultAddress;}
+	void setCalibration(float);
+	void setCalibration(float, float);
 	void setGain(ADS1x15_GAIN_t);
-
-	//float getFullScaleV();
-
+	float getFullScaleV();
+	void setComparatorMode(ADS1x15_COMP_MODE_t);
+	void setComparatorPolarity(ADS1x15_COMP_POL_t);
+	void setComparatorLatch(ADS1x15_COMP_LAT_t);
+	int16_t analogRead(ADS1x15_MUX_t);
 	uint16_t analogRead(uint8_t);
-	uint16_t analogReadVoltage(uint8_t);
-	uint16_t analogReadCurrent(uint8_t);
+	float analogReadVoltage(uint8_t);
+	float analogReadCurrent(uint8_t, float = 100.0);
+	float analogRead420(uint8_t, float = 100.0);
+	inline float getCalibration() {return calibration;}
+	virtual uint8_t getADCbits();
+	virtual uint16_t getFullScaleBits();
 
-private:
+protected:
 	uint16_t configRegister;
+	ADS1x15_GAIN_t currentGain;
+	uint32_t conversionDelay;
+	float calibration;
+	virtual inline uint16_t shiftConversion(uint16_t c) {return c;}
 };
 
+/**
+ * @brief Interface class for the ADS1115 analog to digital converter
+ */
 class ADS1115: public ADS1x15
 {
 public:
-	ADS1115() {ADS1x15();}
+	ADS1115()
+	{
+		ADS1x15();
+		setDataRate(ADS1115_DR_128);
+	}
 	void setDataRate(ADS1115_DR_t);
+	/**
+	 * @brief Get the number of bits of the current ADC
+	 *
+	 * @return Number of bits
+	 */
+	inline uint8_t getADCbits() {return 16;}
+	/**
+	 * @brief Get the full scale binary output for the chip
+	 *
+	 * @return Full scale output
+	 */
+	inline uint16_t getFullScaleBits() {return 0x7FFF;}
 };
 
+/**
+ * @brief Interface class for the ADS1015 analog to digital converter
+ */
 class ADS1015: public ADS1x15
 {
 public:
-	ADS1015() {ADS1x15();}
+	ADS1015()
+	{
+		ADS1x15();
+		setDataRate(ADS1015_DR_1600);
+	}
 	void setDataRate(ADS1015_DR_t);
+	/**
+	 * @brief Get the number of bits of the current ADC
+	 *
+	 * @return Number of bits
+	 */
+	inline uint8_t getADCbits() {return 12;}
+	/**
+	 * @brief Get the full scale binary output for the chip
+	 *
+	 * @return Full scale output
+	 */
+	inline uint16_t getFullScaleBits() {return 0x07FF;}
+private:
+	inline uint16_t shiftConversion(uint16_t c) {return c >> 4;}
 };
 
 
